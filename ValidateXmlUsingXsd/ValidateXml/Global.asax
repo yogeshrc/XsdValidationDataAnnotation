e0ba32ead8b9ecf16c6fbb898650f464bab984b2
Yogesh <yogesh.choudhary@capita.co.uk>
2017-03-28 17:57:19 +0530

    Sample for validating XML using XSD

---
+++ ValidateXmlUsingXsd/ValidateXml/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ValidateXml.MvcApplication" Language="C#" %>
---
+++ ValidateXmlUsingXsd/ValidateXml/Models/Document.cs
@@ -0,0 +1,9 @@
+﻿namespace ValidateXml.Models
+{
+    public class Document
+    {
+        public int Id { get; set; }
+        public string Filename { get; set; }
+        public string FileContents { get; set; }
+    }
+}
